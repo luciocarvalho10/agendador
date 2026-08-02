4289da48350e8cbedebe911fc30c2eefe47b6133
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2026-08-02 17:54:43 -0400

    chore: add shadcn theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import { Button } from "./_components/ui/button";
 
 export default function Home() {
   return (
-    <div className="">Hello World</div>
+    <div className="">
+      <Button>Hello World</Button>
+    </div>
   )
 }
