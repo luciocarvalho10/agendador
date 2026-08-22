d473f64211fec73dd7d50f3116ff98dcf699e5f1
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2026-08-22 17:28:12 -0400

    feat: add Header component search input + button and banner

---
+++ app/_components/header.tsx
@@ -0,0 +1,19 @@
+import { Button } from "./ui/button"
+import { Card, CardContent } from "./ui/card"
+import { MenuIcon } from "lucide-react"
+import Image from "next/image"
+
+const Header = () => {
+  return (
+    <Card>
+      <CardContent className="flex flex-row items-center justify-between p-5">
+        <Image src="/logo.png" alt="Agendador" height={18} width={120} />
+        <Button size="icon" variant="outline">
+          <MenuIcon />
+        </Button>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default Header
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,33 @@
-import { Button } from "./_components/ui/button";
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 export default function Home() {
   return (
     <div className="">
-      <Button>Hello World</Button>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Lucio!</h2>
+        <p>Sabado, 22 de agosto.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça a sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-37.5 w-full">
+          <Image
+            src="/banner-01.png"
+            alt="Agende nos melhores com FSW Barber"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
     </div>
   )
 }
